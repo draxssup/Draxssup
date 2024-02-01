0346a9dbe2c01838ac95c8c119e5ea2b5fc9ba16
draxssup <dhruv.chatrani@gmail.com>
2024-02-01 21:26:27 +0530

    Update README.py

--- README.py
+++ README.py
@@ -2,7 +2,7 @@ class Me:
     def __init__(self):
         self.name = "Draxssup"
         self.current_status = "Lost in Second Year Computer Engineering Wonderland 🤷‍♂️"
-        self.editor = "Definitely not Vim (neovim kinda does count)"
+        self.editor = "Definitely Vim (neovim is for titty lickers)"
         self.future_goal = "Actuarial superstar (maybe?)"
         self.contact = "draxmaxwell@gmail.com"
 
