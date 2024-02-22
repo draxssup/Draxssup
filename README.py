53946d6a2de85e003e187c4745230ced84db12b1
draxssup <dhruv.chatrani@gmail.com>
2024-02-22 22:42:43 +0530

    Update and rename README.py to README.me

--- README.py
+++
@@ -1,8 +0,0 @@
-class Me:
-    def __init__(self):
-        self.name = "Dhruv"
-        self.current_status = "Lost in Second Year Computer Engineering"
-        self.freq_languages= ["Python","R","Julia","Spanish","C"]
-        self.editor = "I use directional keys on no lsp VIM"
-        self.future_goal = "Actuarial"
-        self.contact = "draxmaxwell@gmail.com"
